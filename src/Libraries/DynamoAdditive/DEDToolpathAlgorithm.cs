3918a3b0abebd519971f164a7dbcf017412090f3
Thomas Gale <gale.thomasjames@gmail.com>
2016-12-08 19:10:58 +0000

    Adding prototype dynamo additive library

---
+++ src/Libraries/DynamoAdditive/DEDToolpathAlgorithm.cs
@@ -0,0 +1,71 @@
+﻿//Framework
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+//3rd Party
+using NetfabbAlgorithm;
+using NetfabbAlgorithm.FBYPathGeneration;
+using NetfabbAlgorithm.FBYPathGenerationInterface;
+
+using ManufacturingGeneralTypes.Factory;
+using ManufacturingGeneralTypes.Path;
+
+using ManufacturingAdditiveTypes.Factory.Netfabb;
+using ManufacturingAdditiveTypes.Netfabb.Path;
+
+namespace DynamoAdditive
+{
+    public class DEDToolpathAlgorithm
+    {
+        string _tempDataDirectory = Environment.GetFolderPath(Environment.SpecialFolder.ApplicationData) + @"\Autodesk\Dynamo\Additive\test\tmp";
+
+        internal DEDToolpathAlgorithm()
+        {
+
+        }
+
+        public static DEDToolpathAlgorithm Construct()
+        {
+            return new DEDToolpathAlgorithm();
+        }
+
+
+        public Toolpath CalculateToolpath(string stlFileLocation, DEDBuildStyle selectedBuildStyle)
+        {
+            //Do the Calculation
+            IPathGenerationFactory pathGenerationFactory = new NetfabbPathGenerationFactory(_tempDataDirectory);
+            IPathFactory pathFactory = new ManufacturingGeneralTypes.Factory.Implementation.PathFactory();
+            IAdditivePathFactory additivePathFactory = new ManufacturingAdditiveTypes.Factory.Netfabb.Implementation.AdditivePathFactory();
+
+            ISliceCreator sliceCreator = pathGenerationFactory.ConstructSliceCreator();
+            IFolderCreator folderCreator = pathGenerationFactory.ConstructFolderCreator();
+
+            IPathGenerationModel pathGenerationModel = pathGenerationFactory.ConstructPathGenerationModel(sliceCreator, folderCreator, pathFactory, additivePathFactory);
+
+            IPath path = pathGenerationModel.GeneratePathFromBuildStyle(selectedBuildStyle.BuildStyle, stlFileLocation);
+
+            var toolpathpoints = new List<ToolpathPoint>();
+
+            //Construct Toolpath Object
+            foreach (ILayer layer in path.Layers)
+            {
+                foreach(IExposure exposure in layer.Exposures)
+                {
+                    foreach(ISegment segment in exposure.Segments)
+                    {
+                        foreach(IPoint point in segment.Points)
+                        {
+                            toolpathpoints.Add(ToolpathPoint.FromPoint(point));
+                        }
+                    }
+                }
+            }
+
+            return Toolpath.FromPointsList(toolpathpoints);
+        }
+
+    }
+}
---
+++ src/Libraries/DynamoAdditive/ToolpathPoint.cs
@@ -0,0 +1,71 @@
+﻿//Framework
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+//3rd Party
+using Autodesk.DesignScript.Geometry;
+
+using ManufacturingGeneralTypes.Factory;
+using ManufacturingGeneralTypes.Factory.Implementation;
+
+using ManufacturingGeneralTypes.Path;
+
+namespace DynamoAdditive
+{
+    public class ToolpathPoint
+    {
+        //Path can be internal static object for construction **** Needs re-factoring
+        private static IPathFactory _pathFactory = new PathFactory();
+
+        private IPoint _point;
+
+        internal ToolpathPoint(IPoint point)
+        {
+            _point = point;
+        }
+
+        internal ToolpathPoint(double x, double y, double z)
+        {
+            //Construct a _point object with just XYZ data for the time being
+            _point = _pathFactory.ConstructPoint(
+                _pathFactory.ConstructPointVector(x, y, z, 0, 0, 1, 1, 0, 0),
+                _pathFactory.ConstructPointAxes(),
+                _pathFactory.ConstructPointData()
+                );
+        }
+
+        public static ToolpathPoint FromPoint(IPoint point)
+        {
+            return new ToolpathPoint(point);
+        }
+
+        public static ToolpathPoint FromCoordinates(double x, double y, double z)
+        {
+            return new ToolpathPoint(x, y, z);
+        }      
+
+        public Point AsPoint()
+        {
+            return Point.ByCartesianCoordinates(CoordinateSystem.Identity(), _point.PointVector.X, _point.PointVector.Y, _point.PointVector.Z);
+        }
+
+        public double X
+        {
+            get { return _point.PointVector.X; }
+        }
+
+        public double Y
+        {
+            get { return _point.PointVector.Y; }
+        }
+
+        public double Z
+        {
+            get { return _point.PointVector.Z; }
+        }
+
+    }
+}
